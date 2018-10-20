97a6bb6161281b28cec7388129739898ec19aacb
Alex <alexandrfromminsk@gmail.com>
2018-10-20 17:36:53 -0400

    StackArray ready

--- Stack_and_Queue/StackArray.py
+++ Stack_and_Queue/StackArray.py
@@ -11,5 +11,16 @@ class StackArray:
 
     def is_empty(self):
         return len(self.data)==0
+
     def push(self, val):
         self.data.append(val)
+
+    def top(self):
+        if is_empty():
+            raise Empty("Stack is empty!")
+        else:
+            return self.data[-1]
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty("Stack is empty!")
