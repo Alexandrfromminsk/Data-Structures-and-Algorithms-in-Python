a8c3d225eabdece5429a405a03278fab315cf2c0
Alex <alexandrfromminsk@gmail.com>
2019-01-01 21:25:42 -0500

    Add two inverse int solutions

--- Exercises/solutions/inverse_int.py
+++ Exercises/solutions/inverse_int.py
@@ -4,4 +4,17 @@
 #400 ->4
 
 def inverse_int(i):
-    return i
+    if i>=0:
+        return int(str(i)[::-1])
+    else:
+        return int(str(i)[::-1][:-1])*(-1)
+
+
+def inverse_int2(i):
+    ans=''
+    sign = lambda x: -1 if x < 0 else 1
+    for number in str(i):
+        if number!='-':
+            ans=number+ans
+
+    return int(ans)*sign(i)
--- Exercises/test/test_inverse_int.py
+++ Exercises/test/test_inverse_int.py
@@ -1,10 +1,23 @@
 import unittest
-from Exercises.solutions import inverse_int
+from Exercises.solutions.inverse_int import inverse_int
 
 class TestInverseInt(unittest.TestCase):
 
     def test_simple_int(self):
-        self.AssertTrue(inverse_int(123),321)
+        self.assertEqual(inverse_int(123),321);
+        self.assertEqual(inverse_int(2), 2);
+        self.assertEqual(inverse_int(1234), 4321);
+        self.assertEqual(inverse_int(0), 0);
+
+    def test_with_zeroes(self):
+        self.assertEqual(inverse_int(9000),9);
+        self.assertEqual(inverse_int(700), 7);
+
+    def test_negative(self):
+        self.assertEqual(inverse_int(-123),-321);
+
+    def test_negative_with_zeroes(self):
+        self.assertEqual(inverse_int(-102300),-3201);
 
 
 if __name__ == '__main__':
